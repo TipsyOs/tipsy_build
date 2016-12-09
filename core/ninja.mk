69690543dedee9b2db2298707a6717a6391e799c
Nathan Chancellor <natechancellor@gmail.com>
2016-12-08 19:58:24 -0700

    Fix ninja and ccache not being found
    which throws an error if a binary is not found which breaks the build. Use command -v as it is safer.
    
    Also, switch from ?= to := as it was causing ninja not to be found even with command -v. We'll print what binary the user is using so that there is no confusion either.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- core/ninja.mk
+++ core/ninja.mk
@@ -1,7 +1,13 @@
-NINJA ?= $(shell which ninja)
+NINJA := $(shell command -v ninja)
 ifeq ($(NINJA),)
-  NINJA := prebuilts/ninja/$(HOST_PREBUILT_TAG)/ninja
+ifeq ($(filter address,$(SANITIZE_HOST)),)
+NINJA := prebuilts/build-tools/$(HOST_PREBUILT_TAG)/bin/ninja
+else
+NINJA := prebuilts/build-tools/$(HOST_PREBUILT_TAG)/asan/bin/ninja
 endif
+endif
+
+$(info Using '$(NINJA)' binary on '$(HOST_PREBUILT_TAG)')
 
 ifeq ($(USE_SOONG),true)
 USE_SOONG_FOR_KATI := true
@@ -165,7 +171,7 @@ ifeq ($(KATI_EMULATE_FIND),false)
 endif
 $(KATI_BUILD_NINJA): $(KATI) $(MAKEPARALLEL) $(DUMMY_OUT_MKS) $(SOONG_ANDROID_MK) FORCE
 	@echo Running kati to generate build$(KATI_NINJA_SUFFIX).ninja...
-	+$(hide) $(KATI_MAKEPARALLEL) $(KATI) --ninja --ninja_dir=$(OUT_DIR) --ninja_suffix=$(KATI_NINJA_SUFFIX) --regen --ignore_dirty=$(OUT_DIR)/% --no_ignore_dirty=$(SOONG_ANDROID_MK) --ignore_optional_include=$(OUT_DIR)/%.P --detect_android_echo $(KATI_FIND_EMULATOR) -f build/core/main.mk $(KATI_GOALS) --gen_all_targets BUILDING_WITH_NINJA=true SOONG_ANDROID_MK=$(SOONG_ANDROID_MK)
+	+$(hide) $(KATI_MAKEPARALLEL) $(KATI) --ninja --ninja_dir=$(OUT_DIR) --ninja_suffix=$(KATI_NINJA_SUFFIX) --regen --ignore_dirty=$(OUT_DIR)/% --no_ignore_dirty=$(SOONG_ANDROID_MK) --ignore_optional_include=$(OUT_DIR)/%.P $(KATI_FIND_EMULATOR) -f build/core/main.mk $(KATI_GOALS) --gen_all_targets BUILDING_WITH_NINJA=true SOONG_ANDROID_MK=$(SOONG_ANDROID_MK)
 
 ifneq ($(USE_SOONG_FOR_KATI),true)
 KATI_CXX := $(CLANG_CXX) $(CLANG_HOST_GLOBAL_CFLAGS) $(CLANG_HOST_GLOBAL_CPPFLAGS)
