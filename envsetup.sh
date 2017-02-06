52d95333aecb2f5b79c43db8e3f196189261ebdc
Martinusbe <martinusbe@gmail.com>
2017-02-06 17:06:53 +0100

    envsetup: exlude generic devices

--- envsetup.sh
+++ envsetup.sh
@@ -1694,7 +1694,7 @@ if ! __detect_shell > /dev/null; then
 fi
 
 # Execute the contents of any vendorsetup.sh files we can find.
-for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
+for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' ! -path '*/generic/*' 2> /dev/null | sort` \
          `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
          `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
 do
